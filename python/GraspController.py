32b29f10291bd6229ed568d22e78b2bf2ea0d5ec
LindavdSpaa <lindavanderspaa@hotmail.com>
2022-07-28 12:58:30 +0200

    Changed grasp to force control via franka_gripper/grasp

--- python/GraspController.py
+++ python/GraspController.py
@@ -11,55 +11,84 @@ l.f.vanderspaa@tudelft.nl
 import rospy, time
 from std_msgs.msg import Float32, Bool, Int8
 from sensor_msgs.msg import JointState
+from franka_gripper.msg import GraspActionGoal
 
 class GraspController:
   def gripper_pos_callback(self, data):
     pos = data.position[0]
-    # Object of a width between 2 and 20 mm
-    self.curr_state = 0 if pos > 0.01 else 1 if pos > 0.001 else -1
+    # Object of a width between 6 and 10 mm
+    self.curr_state = 0 if pos > 0.01/2 else 1 if pos > 0.006/2 else -1
     self.state_pub.publish(self.curr_state)
 
-  def open(self, dOpen=0.1): # open distance [m]
-    self.gripper_pub.publish(dOpen)
+  def open(self, dOpen=0.01): # open distance [m]
+    if self.last_grip_command == False:
+      # exit without acting if previous command sent was the same
+      return 0 if self.curr_state == 0 else -1
 
-    while True:
+    self.grip_command.goal.epsilon.inner = 0.0
+    self.grip_command.goal.epsilon.outer = 0.0
+    self.grip_command.goal.force = 0.
+    self.grip_command.goal.width = dOpen
+    self.gripper_pub.publish(self.grip_command)
+    self.last_grip_command = False
+
+    r = rospy.Rate(20)
+    while not rospy.is_shutdown():
       if self.curr_state == 0:
-        time.sleep(0.5)
         return 0  # exit flag
-      time.sleep(0.05)
+      r.sleep()
 
   def close(self, dClose=0.0): # close distance [m]
-    self.gripper_pub.publish(dClose)
+    if self.last_grip_command:
+      # exit without acting if previous command sent was the same
+      return 0 if self.curr_state else -1
 
-    while True:
+    self.grip_command.goal.epsilon.inner = 0.1
+    self.grip_command.goal.epsilon.outer = 0.1
+    self.grip_command.goal.force = 1000.
+    self.grip_command.goal.width = dClose
+    self.gripper_pub.publish(self.grip_command)
+    self.last_grip_command = True
+
+    r = rospy.Rate(20)
+    while not rospy.is_shutdown():
       if self.curr_state:
-        time.sleep(0.2)
         return 0  # exit flag
-      time.sleep(0.05)
+      r.sleep()
 
   def grasp_callback(self, grasp):
     self.goal_state = 1 if grasp.data else 0
 
   def __init__(self) -> None:
     self.goal_state = 0
-    self.gripper_pub = rospy.Publisher('/gripper_online', Float32, queue_size=0)
+
+    self.grip_command = GraspActionGoal()
+    self.grip_command.goal.speed = 1.
+    self.grip_command.goal.epsilon.inner = 0.
+    self.grip_command.goal.epsilon.outer = 0.
+    self.grip_command.goal.force = 0.
+    self.grip_command.goal.width = 0.1
+    self.last_grip_command = None
+
     self.state_pub = rospy.Publisher('/grasp_controller/state', Int8, queue_size=0)
+    self.gripper_pub = rospy.Publisher("franka_gripper/grasp/goal", GraspActionGoal, queue_size=0)
 
     rospy.Subscriber("/franka_gripper/joint_states", JointState, self.gripper_pos_callback)
     rospy.Subscriber("/grasp_controller/grasp", Bool, self.grasp_callback)
     time.sleep(1)
 
   def run(self) -> None:
-    r = rospy.Rate(1)
+    r = rospy.Rate(20)
     while not rospy.is_shutdown():
-      if self.goal_state == 0 and self.curr_state != 0:
+      if self.goal_state == 0 and self.curr_state != 0 and self.last_grip_command != False:
         self.open()
       
       elif self.goal_state == 1 and self.curr_state != self.goal_state:
-        if self.curr_state < 0:
+        if self.curr_state < 0 and self.last_grip_command:
           print("Warning: lost the object, trying to regrasp")
           self.open()
-        self.close()
+        elif self.curr_state == 0 and self.last_grip_command != True:
+          self.close()
 
       r.sleep()
 
