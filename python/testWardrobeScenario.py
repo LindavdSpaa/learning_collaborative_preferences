420e6d4597b7655ff9894711f2abbe509d2934f2
LindavdSpaa <lindavanderspaa@hotmail.com>
2022-08-05 17:03:06 +0200

    testWardrobeScenario: save learning data

--- python/testWardrobeScenario.py
+++ python/testWardrobeScenario.py
@@ -1,4 +1,5 @@
-import rospy, time, os
+import rospy, time, os, itertools
+from datetime import datetime
 from std_msgs.msg import Int16, String
 import numpy as np
 
@@ -20,11 +21,32 @@ robotPolicy = 'learner'  # 'learner', 'imitator'
 if robotPolicy == 'learner':
   learner = IAPreferenceLearner(scenario)
 
+  n_intentions = scenario.intentionsSet.n
+  n_stateActions = sum([len(sa) for sa in scenario.actionsSet])
+  learnedPolicy = np.zeros((30,n_intentions,n_stateActions))
+
+  for i in range(n_intentions):
+    intention = np.zeros(n_intentions)
+    intention[i] = 1
+    learnedPolicy[0,i] = list(itertools.chain(*[learner.robot.getActionDistribution(s,intention) for s in range(scenario.statesSet.n)]))
+elif robotPolicy == 'imitator':
+  learnedPolicy = []
+
+trajectoryTraces = []
+intentionsToTraces = []
+beliefsTraces = []
+k = 0
+
 #%% Start with an open gripper
 actionController.grasp(False)
 
 #%%
 actionController.goToGoal(1)
+
+
+
+
+
 
 #%%
 state, stateIdx = actionController.goToClosestGoal()
@@ -61,14 +83,29 @@ while not scenario.isSupportState(state) or not len(stateTrace):
   state = nextState
 stateTrace.append(state)
 
-trajectoryTraces += [[[stateTrace[i], beliefTrace[i], robotActionTrace[i], humanActionTrace[i], stateTrace[i+1]] for i in range(len(robotActionTrace))]]
-intentionsToTraces += [scenario.getIntentionIdx(stateTrace[-1][:-1])]
-
-#%%
+##%%
 if robotPolicy == 'imitator':
   for i in range(len(humanActionTrace)):
     scenario.updateActionImitationList(stateTrace[i], humanActionTrace[i])
+  trajectoryTraces += [[[stateTrace[i], robotActionTrace[i], humanActionTrace[i], stateTrace[i+1]] for i in range(len(robotActionTrace))]]
+  learnedPolicy += [scenario.stateActionImitationList]
 elif robotPolicy == 'learner':
+  trajectoryTraces += [[[stateTrace[i], beliefTrace[i], robotActionTrace[i], humanActionTrace[i], stateTrace[i+1]] for i in range(len(robotActionTrace))]]
+  intentionsToTraces += [scenario.getIntentionIdx(stateTrace[-1][:-1])]
+  beliefsTraces += [beliefTrace]
   learner.updateModel(trajectoryTraces[-1:], intentionsToTraces[-1:])
 
+  k += 1
+  for i in range(n_intentions):
+    intention = np.zeros(n_intentions)
+    intention[i] = 1
+    learnedPolicy[k,i] = list(itertools.chain(*[learner.robot.getActionDistribution(s,intention) for s in range(scenario.statesSet.n)]))
+
+
+# %%
+np.savez(datetime.now().strftime("%Y-%m-%d-%H-%M-%S")+'-learned-policy-'+robotPolicy,
+          trajectories=np.array(trajectoryTraces, dtype=object),
+          intentions=np.array(intentionsToTraces, dtype=object), 
+          beliefs=np.array(beliefsTraces, dtype=object), 
+          learnedPolicy=np.array(learnedPolicy, dtype=object))
 # %%
